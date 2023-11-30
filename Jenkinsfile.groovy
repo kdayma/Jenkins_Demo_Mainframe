08d16b7e97fc4820c9821eaa17c3068da81d8d69
Keshav Dayma <kdayma@rocketsoftware.com>
2023-11-30 23:51:45 +0530

    Required files updated.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,13 +9,26 @@ pipeline {
                         }
                     }
             }
+            stage('Packaging') {
+                steps{
+                    script {
+                        build job: 'Package_Dataset'
+                        jiraAssignIssue site:'Rocket Innovation Day', idOrKey: params.Packaging_Id, userName : 'kdayma'
+                        packageComment = [body:"Package Dataset TS4866.TEMP.ABJTLOAD TS4866.TEMP.ABJTSAMP"]
+                        jiraAddComment site: 'Rocket Innovation Day', idOrKey: params.Packaging_Id, input: packageComment, auditLog: false
+                    }
+                }
+            }
 			stage('Jira') {
 				steps {
 					script {
 							successComment = [body:" Successfully scanned"]
-							
+							def id = params.JiraID
+							if (id.isEmpty()){
+							    echo "Id is empty"
+							}
 							echo "We are successful in JIRA with success comment"
-							jiraAddComment site: 'Rocket Innovation Day', idOrKey: 'RID-5', input: successComment, auditLog: false
+							jiraAddComment site: 'Rocket Innovation Day', idOrKey: params.JiraID, input: successComment, auditLog: false
 							echo "Comment published"
 					}
 				}
@@ -25,7 +38,7 @@ pipeline {
 					script {
 						echo "Integrating Slack Message"
 						slackSend color: "good", message: "Message from Jenkins Pipeline got completed..."
-						slackSend(channel: "Keshav Dayma", message: "Message from Jenkins Pipeline got completed...")
+						slackSend(channel: params.SlackUserId, message: "Message from Jenkins Pipeline got completed...")
 						echo "Slack Message send"
 					}
 				}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-((((( Jenkins mainframe Demo with rexx codes for testing purposes ))))) Thu 11/30/2023 15:15:08.36
+((((( Jenkins mainframe Demo with rexx codes for testing purposes ))))) Thu 11/30/2023 23:51:44.57
